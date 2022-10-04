0fedafd3f8df446c89bc6dbe9a1551efc607b459
yaden-s <denisgubin@yahoo.com>
2022-10-04 18:16:25 +0300

    Add solution to exercise 10.5

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,8 +45,7 @@ class User < ApplicationRecord
 
     # Активирует учетную запись. 
     def activate
-        update_attribute(:activated, true)
-        update_attribute(:activated_at, Time.zone.now)
+        update_columns(activated: true, activated_at: Time.zone.now)
     end
 
     # Посылает письмо со ссылкой на страницу активации.
@@ -57,8 +56,7 @@ class User < ApplicationRecord
     # Устанавливает атрибуты для сброса пароля. 
     def create_reset_digest
         self.reset_token = User.new_token 
-        update_attribute(:reset_digest, User.digest(reset_token)) 
-        update_attribute(:reset_sent_at, Time.zone.now)
+        update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
     end
 
     # Посылает письмо со ссылкой на форму ввода нового пароля. 
